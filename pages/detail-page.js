d6c88c22d9ad67856f74799b73bdacac47c72127
Alena <elen.malets@gmail.com>
2019-04-26 14:12:24 +0200

    Display elements

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,28 +1,24 @@
-console.log(document.getElementById('name'));
+function submitComment() {
+    // gather info
+    const inputField = document.getElementById('name');
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
 
+    //create elements
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
 
-// function submitComment() {
-//     // gather info
-//     const inputField = document.getElementById('name');
-// const name = inputField.value;
-// const textArea = document.getElementById('msg');
-// const msg = textArea.value;
+    //combine data with elements
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
 
-// //create elements
-// const comment = document.createElement('section');
-// const h3 = document.createElement('h3');
-// const p = document.createElement('p');
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
 
-// //combine data with elements
-// h3.innerHTML = `${name} said:`;
-// p.innerHTML = msg;
-// comment.classList.add('comment');
-// comment.appendChild(h3);
-// comment.appendChild(p);
-// console.log(comment);
-
-// const commentSection = document.getElementById('comments');
-// console.log(commentSection);
-
-// }
-// submitComment()
+}
+submitComment()
